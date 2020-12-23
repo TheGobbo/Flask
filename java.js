1e710dc0b8c8f6df0d400d20a631b979421a9cce
gobboE <gobbolegal@gmail.com>
2020-12-22 21:15:02 -0300

    fixing errors

--- java.js
+++ java.js
@@ -1,13 +1,15 @@
 let counter = 0;
 
-function pop() {
-    let name = document.querySelector("#name").value;
-    if (name === null){
-        name = 'world';
-    }
-    alert('Hello ' + name + ' !');
-
 function increment(){
     counter++;
     document.querySelector("#star").innerHTML = counter;
 }
+
+function pop() {
+    let name = document.querySelector("#name").value;
+    if (name === null) {
+        name = 'world';
+    }
+    alert('Hello ' + name + ' !');
+}
+
